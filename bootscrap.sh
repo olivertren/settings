a7b299ad0aec0dabe34061b7c7968e37a996e2da
Oliver Ren <oliver.ren@klaviyo.com>
2022-02-02 10:40:52 -0500

    add in bootstrap settings

---
+++ bootscrap.sh
@@ -0,0 +1,27 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install bat
+brew install broot
+brew install diff-so-fancy
+brew install exa
+brew install fasd
+brew install fd
+brew intsall freetype
+brew install fzf
+brew install git
+brew install glances
+brew install jq
+brew install ripgrep
+brew install tldr
+brew install zplug
+
+
+brew install pyenv
+brew install pyenv-virtualenv
+brew install readline
+brew install openssl@1.1
+brew install openssl@3
+brew install freetype
+
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono-nerd-font
